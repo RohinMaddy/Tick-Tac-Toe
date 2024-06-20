03244f1dc0bdde019eb13b5f625d247e9974f3b9
Rohin Madhavan <rohinmadhavan96@gmail.com>
2024-06-20 12:54:19 +0100

    Initial Commit

---
+++ Tick Tac Toe/Tick_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tick_Tac_ToeApp.swift
+//  Tick Tac Toe
+//
+//  Created by Rohin Madhavan on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tick_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
